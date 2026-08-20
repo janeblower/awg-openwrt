d61508d4b0d19a4ef51d8dbf5ea3b199ddb14b54
Jyn <iamnotjyn@gmail.com>
2026-08-20 20:21:34 +0700

    fix: install AWG 3.1 packages on OpenWrt 25.x only

--- amneziawg-install.sh
+++ amneziawg-install.sh
@@ -133,25 +133,19 @@ install_awg_packages() {
     SUBTARGET=$(ubus call system board | jsonfilter -e '@.release.target' | cut -d '/' -f 2)
     VERSION=$(ubus call system board | jsonfilter -e '@.release.version')
     PKGPOSTFIX_BASE="_v${VERSION}_${PKGARCH}_${TARGET}_${SUBTARGET}"
-    BASE_URL="https://github.com/Slava-Shchipunov/awg-openwrt/releases/download/"
+    BASE_URL="https://github.com/janeblower/awg-openwrt/releases/download/"
 
-    # Определяем версию AWG протокола (2.0 для OpenWRT >= 23.05.6 и >= 24.10.3)
-    AWG_VERSION="1.0"
+    # В релизах этого репозитория лежат пакеты AWG 3.1 только для OpenWRT 25.x и новее
     MAJOR_VERSION=$(echo "$VERSION" | cut -d '.' -f 1)
-    MINOR_VERSION=$(echo "$VERSION" | cut -d '.' -f 2)
-    PATCH_VERSION=$(echo "$VERSION" | cut -d '.' -f 3)
 
-    if [ "$MAJOR_VERSION" -gt 24 ] || \
-       [ "$MAJOR_VERSION" -eq 24 -a "$MINOR_VERSION" -gt 10 ] || \
-       [ "$MAJOR_VERSION" -eq 24 -a "$MINOR_VERSION" -eq 10 -a "$PATCH_VERSION" -ge 3 ] || \
-       [ "$MAJOR_VERSION" -eq 23 -a "$MINOR_VERSION" -eq 5 -a "$PATCH_VERSION" -ge 6 ]; then
-        AWG_VERSION="2.0"
-        LUCI_PACKAGE_NAME="luci-proto-amneziawg"
-    else
-        LUCI_PACKAGE_NAME="luci-app-amneziawg"
+    if [ "$MAJOR_VERSION" -lt 25 ]; then
+        printf "\033[32;1mOpenWRT $VERSION is not supported: AWG 3.1 packages are built for OpenWRT 25.x and newer\033[0m\n"
+        exit 1
     fi
 
-    printf "\033[32;1mDetected AWG version: $AWG_VERSION\033[0m\n"
+    LUCI_PACKAGE_NAME="luci-proto-amneziawg"
+
+    printf "\033[32;1mInstalling AmneziaWG 3.1 packages for OpenWRT $VERSION\033[0m\n"
 
     AWG_DIR="/tmp/amneziawg"
     mkdir -p "$AWG_DIR"
@@ -199,7 +193,7 @@ install_awg_packages() {
     fi
 
     # Проверяем оба возможных названия пакета
-    if is_pkg_installed "luci-proto-amneziawg" || is_pkg_installed "luci-app-amneziawg"; then
+    if is_pkg_installed "luci-proto-amneziawg"; then
         echo "$LUCI_PACKAGE_NAME already installed"
     else
         LUCI_AMNEZIAWG_FILENAME=$(download_package "$LUCI_PACKAGE_NAME" "$PKGPOSTFIX_BASE" "$AWG_DIR" "${BASE_URL}v${VERSION}/")
@@ -220,8 +214,8 @@ install_awg_packages() {
         fi
     fi
 
-    # Устанавливаем русскую локализацию только для AWG 2.0
-    if [ "$AWG_VERSION" = "2.0" ] && [ $ASK_FOR_TRANSLATION = 1 ]; then
+    # Русская локализация
+    if [ $ASK_FOR_TRANSLATION = 1 ]; then
         printf "\033[32;1mУстанавливаем пакет с русской локализацией? Install Russian language pack? (y/n) [n]: \033[0m\n"
         read INSTALL_RU_LANG
         INSTALL_RU_LANG=${INSTALL_RU_LANG:-n}
@@ -288,16 +282,13 @@ configure_amneziawg_interface() {
     read -r -p "Enter H3 value (from [Interface]):"$'\n' AWG_H3
     read -r -p "Enter H4 value (from [Interface]):"$'\n' AWG_H4
 
-    # AWG 2.0 новые параметры
-    if [ "$AWG_VERSION" = "2.0" ]; then
-        read -r -p "Enter S3 value (from [Interface]) [optional, leave blank to skip]:"$'\n' AWG_S3
-        read -r -p "Enter S4 value (from [Interface]) [optional, leave blank to skip]:"$'\n' AWG_S4
-        read -r -p "Enter I1 value (from [Interface]) [optional, leave blank to skip]:"$'\n' AWG_I1
-        read -r -p "Enter I2 value (from [Interface]) [optional, leave blank to skip]:"$'\n' AWG_I2
-        read -r -p "Enter I3 value (from [Interface]) [optional, leave blank to skip]:"$'\n' AWG_I3
-        read -r -p "Enter I4 value (from [Interface]) [optional, leave blank to skip]:"$'\n' AWG_I4
-        read -r -p "Enter I5 value (from [Interface]) [optional, leave blank to skip]:"$'\n' AWG_I5
-    fi
+    read -r -p "Enter S3 value (from [Interface]) [optional, leave blank to skip]:"$'\n' AWG_S3
+    read -r -p "Enter S4 value (from [Interface]) [optional, leave blank to skip]:"$'\n' AWG_S4
+    read -r -p "Enter I1 value (from [Interface]) [optional, leave blank to skip]:"$'\n' AWG_I1
+    read -r -p "Enter I2 value (from [Interface]) [optional, leave blank to skip]:"$'\n' AWG_I2
+    read -r -p "Enter I3 value (from [Interface]) [optional, leave blank to skip]:"$'\n' AWG_I3
+    read -r -p "Enter I4 value (from [Interface]) [optional, leave blank to skip]:"$'\n' AWG_I4
+    read -r -p "Enter I5 value (from [Interface]) [optional, leave blank to skip]:"$'\n' AWG_I5
 
     uci set network.${INTERFACE_NAME}=interface
     uci set network.${INTERFACE_NAME}.proto=$PROTO
@@ -315,16 +306,14 @@ configure_amneziawg_interface() {
     uci set network.${INTERFACE_NAME}.awg_h3=$AWG_H3
     uci set network.${INTERFACE_NAME}.awg_h4=$AWG_H4
 
-    # Устанавливаем новые параметры для AWG 2.0 (только если они заданы)
-    if [ "$AWG_VERSION" = "2.0" ]; then
-        [ -n "$AWG_S3" ] && uci set network.${INTERFACE_NAME}.awg_s3=$AWG_S3
-        [ -n "$AWG_S4" ] && uci set network.${INTERFACE_NAME}.awg_s4=$AWG_S4
-        [ -n "$AWG_I1" ] && uci set network.${INTERFACE_NAME}.awg_i1=$AWG_I1
-        [ -n "$AWG_I2" ] && uci set network.${INTERFACE_NAME}.awg_i2=$AWG_I2
-        [ -n "$AWG_I3" ] && uci set network.${INTERFACE_NAME}.awg_i3=$AWG_I3
-        [ -n "$AWG_I4" ] && uci set network.${INTERFACE_NAME}.awg_i4=$AWG_I4
-        [ -n "$AWG_I5" ] && uci set network.${INTERFACE_NAME}.awg_i5=$AWG_I5
-    fi
+    # Новые параметры записываем только если они заданы
+    [ -n "$AWG_S3" ] && uci set network.${INTERFACE_NAME}.awg_s3=$AWG_S3
+    [ -n "$AWG_S4" ] && uci set network.${INTERFACE_NAME}.awg_s4=$AWG_S4
+    [ -n "$AWG_I1" ] && uci set network.${INTERFACE_NAME}.awg_i1=$AWG_I1
+    [ -n "$AWG_I2" ] && uci set network.${INTERFACE_NAME}.awg_i2=$AWG_I2
+    [ -n "$AWG_I3" ] && uci set network.${INTERFACE_NAME}.awg_i3=$AWG_I3
+    [ -n "$AWG_I4" ] && uci set network.${INTERFACE_NAME}.awg_i4=$AWG_I4
+    [ -n "$AWG_I5" ] && uci set network.${INTERFACE_NAME}.awg_i5=$AWG_I5
 
     if ! uci show network | grep -q ${CONFIG_NAME}; then
         uci add network ${CONFIG_NAME}
